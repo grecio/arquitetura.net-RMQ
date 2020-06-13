9250edc46a8f077ff0869b377cc19838e1f4b972
grecio@gmail.com <buga@tec_buga1>
2020-06-13 02:00:24 -0300

    atuando no domain

--- slMicroRabbitMQ/MicroRabbitMQ.Banking.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MicroRabbitMQ.Banking.Application
-{
-    public class Class1
-    {
-    }
-}
--- slMicroRabbitMQ/MicroRabbitMQ.Banking.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MicroRabbitMQ.Banking.Data
-{
-    public class Class1
-    {
-    }
-}
