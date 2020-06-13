d17018909c006c3db0307990a60c64d791f6e7d7
grecio@gmail.com <buga@tec_buga1>
2020-06-13 01:45:03 -0300

    Resovendo dependencias usando Microsoft Extensions

---
+++ slMicroRabbitMQ/MicroRabbitMQ.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MicroRabbitMQ.Domain.Core.Bus;
+using MicroRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbitMQ.Infra.IoC
+{
+    public class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
