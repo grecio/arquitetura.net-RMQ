1952e64136883476252aa2e782b37eaacebb32ca
grecio@gmail.com <buga@tec_buga1>
2020-06-12 23:44:45 -0300

    initial commt

---
+++ slArquiteturaRMQ/Producer/Sender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var message = "Getting Started with .net core and RadbbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent a Message {0}", message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
