ff445eba87fb30f85564bc553cad276d522d4b1f
grecio@gmail.com <buga@tec_buga1>
2020-06-16 01:32:31 -0300

    Ajustes no context

--- slMicroRabbitMQ/MicroRabbitMQ.Banking.Data/Context/BankingDbContext.cs
+++ slMicroRabbitMQ/MicroRabbitMQ.Banking.Data/Context/BankingDbContext.cs
@@ -6,7 +6,8 @@ namespace MicroRabbitMQ.Banking.Data.Context
 {
     public class BankingDbContext : DbContext
     {
-        public BankingDbContext(DbContextOptions options) : base(options)
+       
+        public BankingDbContext(DbContextOptions<BankingDbContext> options) : base(options)
         {
 
         }
